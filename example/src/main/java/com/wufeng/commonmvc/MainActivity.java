feb7880a43db38aebfa51330bac38bc45ff21799
wufeng <wufeng13524474117.163.com>
2020-04-28 10:06:54 +0800

    添加框架。吴峰

---
+++ example/src/main/java/com/wufeng/commonmvc/MainActivity.java
@@ -0,0 +1,54 @@
+package com.wufeng.commonmvc;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+                }else {
+                    call();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 1){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                call();
+            else
+                LogUtil.d("MainActivity", "call denied");
+
+        }
+    }
+
+    private void call(){
+        try{
+            Intent intent = new Intent(Intent.ACTION_CALL);
+            intent.setData(Uri.parse("tel:10086"));
+            startActivity(intent);
+        }catch (SecurityException se){
+            se.printStackTrace();
+        }
+    }
+}
---
+++ example/src/main/java/com/wufeng/commonmvc/MyApplication.java
@@ -0,0 +1,17 @@
+package com.wufeng.commonmvc;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    private static Context context;
+
+    public static Context getContext(){
+        return context;
+    }
+
+    @Override
+    public void onCreate() {
+        context = getApplicationContext();
+    }
+}
