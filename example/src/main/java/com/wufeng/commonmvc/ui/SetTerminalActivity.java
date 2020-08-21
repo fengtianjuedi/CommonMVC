c0c70e22bd09965dd23c5d0b1e10a861f5137881
wufeng <wufeng13524474117.163.com>
2020-08-21 18:05:17 +0800

    添加终端设置页面。吴峰

---
+++ example/src/main/java/com/wufeng/commonmvc/ui/SetTerminalActivity.java
@@ -0,0 +1,17 @@
+package com.wufeng.commonmvc.ui;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wufeng.commonmvc.databinding.ActivitySetTerminalBinding;
+import com.wufeng.latte_core.activity.BaseActivity;
+
+public class SetTerminalActivity extends BaseActivity<ActivitySetTerminalBinding> {
+
+    @Override
+    protected void init(@Nullable Bundle savedInstanceState) {
+
+    }
+}
