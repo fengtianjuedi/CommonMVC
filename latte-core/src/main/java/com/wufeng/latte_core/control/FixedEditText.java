6b43f0da778b13426924cd45d7f37d767bcde84a
wufeng <wufeng13524474117.163.com>
2020-08-24 18:16:15 +0800

    添加自定义文本编辑控件。吴峰

--- latte-core/src/main/java/com/wufeng/latte_core/control/FixedEditText.java
+++ latte-core/src/main/java/com/wufeng/latte_core/control/FixedEditText.java
@@ -1,27 +1,61 @@
 package com.wufeng.latte_core.control;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatEditText;
 
+import com.wufeng.latte_core.R;
+
 public class FixedEditText extends AppCompatEditText {
     private String fixedText;
     private int leftPadding;
+    private Paint fixedTextPaint;
 
     public FixedEditText(@NonNull Context context) {
         super(context);
+        fixedTextPaint = new Paint();
+        fixedTextPaint.setAntiAlias(true);
+        fixedTextPaint.setDither(true);
+        fixedTextPaint.setTextSize(getTextSize());
+        fixedTextPaint.setColor(getCurrentTextColor());
     }
 
     public FixedEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        fixedTextPaint = new Paint();
+        fixedTextPaint.setAntiAlias(true);
+        fixedTextPaint.setDither(true);
+        fixedTextPaint.setTextSize(getTextSize());
+        fixedTextPaint.setColor(getCurrentTextColor());
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.FixedEditText);
+        String fixedText = typedArray.getString(R.styleable.FixedEditText_fixedText);
+        setFixedText(fixedText);
+        typedArray.recycle();
     }
 
-    public FixedEditText(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int left = getPaddingLeft();
+        int textWidth = (int) (getPaint().measureText(getText().toString()));
+
+        if (!TextUtils.isEmpty(fixedText)){
+            canvas.drawText(fixedText, leftPadding, getBaseline(), fixedTextPaint);
+        }
     }
 
-
+    public void setFixedText(String text){
+        fixedText = text;
+        leftPadding = getPaddingLeft();
+        int left = (int) (getPaint().measureText(text) + leftPadding + 6);
+        setPadding(left, getPaddingTop(), getPaddingRight(), getPaddingBottom());
+        invalidate();
+    }
 }
